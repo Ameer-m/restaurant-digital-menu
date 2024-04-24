37eeb0f970092c72f1f50eeaf19b90d7b050e5bc
Ameer-m <ameerahmedkhan.m@gmail.com>
2024-04-24 15:25:33 +0400

    add bahrain

--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -3,13 +3,14 @@ import { HomePage } from "../pages/HomePage";
 import { MenuPage } from "../pages/MenuPage";
 import NotFoundPage from "../pages/NotFoundPage"; // Import the NotFoundPage
 import Loader from '../components/Loader';
+import Bahrain from '../pages/Bahrain';
 
 const Index = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/bahrain" element={<MenuPage />} />
+        <Route path="/bahrain" element={<Bahrain />} />
         <Route path="/:slug/" element={<MenuPage />} />
         <Route path="/404" element={<NotFoundPage />} /> {/* Catch-all route for 404 */}
       </Routes>
