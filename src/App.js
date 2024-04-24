1b771fcfea99d2cf0ce1ecc2a1c0a7f9629e0f6a
Ameer-m <ameerahmedkhan.m@gmail.com>
2024-04-24 16:42:04 +0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import MyRouter from "./router/Index";
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { LanguageProvider } from './context/LanguageContext';
+import { HomePage } from "./pages/HomePage";
+import { MenuPage } from "./pages/MenuPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <LanguageProvider>
-    <div className="bg-white">
-      <MyRouter />
-    </div>
+     <BrowserRouter>
+        <Routes>      
+          <Route path="/" element={<HomePage/>} />
+          {/* <Route path="/bahrain" element={<Bahrain />} /> */}
+          <Route path="/:slug" element={<MenuPage />} />
+          <Route path="/404" element={<NotFoundPage />} /> {/* Catch-all route for 404 */}
+        </Routes>
+      </BrowserRouter>
     </LanguageProvider>
   );
 }
