242d6f18dc900cc5ed61115ee4a76c37d3d0c5d4
Ameer-m <ameerahmedkhan.m@gmail.com>
2024-04-24 17:57:05 +0400

    udpate

--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -2,7 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomePage } from "../pages/HomePage";
 import { MenuPage } from "../pages/MenuPage";
 import NotFoundPage from "../pages/NotFoundPage"; // Import the NotFoundPage
-import Loader from '../components/Loader';
+// import Loader from '../components/Loader';
+
+const RemoveTrailingSlash = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  if (location.pathname.endsWith('/') && location.pathname !== '/') {
+    const newPath = location.pathname.slice(0, -1);
+    history.replace(newPath);
+    return null;
+  }
+
+  return null;
+};
 
 const Index = () => {
   return (
